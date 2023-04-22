b13ee926aeb5f35fee1188df71d098549b5dfc64
Jessica Bring <jbring@ad.unc.edu>
2023-04-21 20:24:52 -0400

    dummy

--- app.py
+++ app.py
@@ -15,19 +15,19 @@ def index():
 @app.route("/joke")
 def my_jokes():
     # intialize a variable a_joke and call the API function get_a_joke()
-    a_joke: dict[str, str] = get_a_joke()
+    a_joke: dict[str, str] = ...
 
     # Now, we'll render the designated HTML template, 'jokes.html' . Then will the setup and punchline values to the template
     # use the keys "setup" and "punchline" to access the value associated with that key -- this is the informat we want to display!
 
     # Ex: setup=a_joke["setup"]
-    return render_template('joke.html', setup= a_joke["setup"], punchline=a_joke["punchline"])
+    return render_template('joke.html', setup= a_joke[...], punchline=a_joke[...])
 
 
 @app.route("/many-jokes")
 def many_jokes():
     # intialize a variable ten_jokes and call the API function get_10_jokes() (TAKE NOTE OF THE TYPE IT RETURNS)
-    ten_jokes: list[dict[str, str]] = get_10_jokes()
+    ten_jokes: list[dict[str, str]] = ...
 
     # Now, we'll render the designated HTML template, 'many-jokes.html' . Then will the setup and punchline values to the template
     # This time, we'll pass in the whole list / ten_jokes variable to the template
@@ -46,11 +46,11 @@ def get_a_joke() -> dict[str, str]:
     # don't worry about the type, Python will take care of this
 
     # use requests.get() and pass in the API URL variable -- this will fetch a JSON
-    data = requests.get(jokes_api_url)
+    data = ...
 
     # call .json() on the data variable - this will parse the JSON to a dict[str,str]
     # be careful of json structure -- sometimes it can be formatted within a list!
-    response: dict[str, str] = data.json()
+    response: dict[str, str] = ...
 
     return response
 
@@ -65,11 +65,11 @@ def get_10_jokes() -> list[dict[str, str]]:
     # don't worry about the type, Python will take care of this
 
     # use requests.get() and pass in the API URL variable -- this will fetch a JSON
-    data = requests.get(jokes_api_url)
+    data = ...
 
     # # call .json() on the data variable - this will parse the JSON
     # The response JSON is now a LIST of dictionaries! It's important to know the structure of response JSON!
-    response: list[dict[str, str]] = data.json()
+    response: list[dict[str, str]] = ...
 
     return response
 
@@ -79,15 +79,15 @@ def get_pictures() -> dict[str, str]:
     # We'll be using Mars Rover images from NASA: https://api.nasa.gov/index.html
 
     # Follow along with the documentation and create an account to generate a unique app id and key
-    app_key = "lLeOXVNpsKNIUQynvCtxwdhxeDWQwR8VWVZmnN92"
+    app_key = "<API KEY>"
 
 
 
-    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=lLeOXVNpsKNIUQynvCtxwdhxeDWQwR8VWVZmnN92"
-    data = requests.get(url)
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=<API KEY>"
+    data = ...
 
     # call .json() on data
-    response = data.json()
+    response = ...
 
     return response
 
@@ -102,7 +102,7 @@ def my_pictures():
     num = random.randint(1, 3)
 
     # Call get_pictures()
-    pictures: dict[str, str] = get_pictures()
+    pictures: dict[str, str] = ...
     # the dictionary returned from this API call is a lot more complicated than previous examples we've shown, notice the keys and indicing below
     return render_template('mars.html', source=pictures['photos'][num]['img_src'], date=pictures['photos'][num]['earth_date'])
 
