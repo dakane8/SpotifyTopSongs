7f91728db940bd898d93896d6487989c17b9fb02
Jessica Bring <jbring@ad.unc.edu>
2023-04-21 20:14:57 -0400

    final changes

--- app.py
+++ app.py
@@ -15,36 +15,24 @@ def index():
 @app.route("/joke")
 def my_jokes():
     # intialize a variable a_joke and call the API function get_a_joke()
-    a_joke: dict[str, str] = ...
+    a_joke: dict[str, str] = get_a_joke()
 
     # Now, we'll render the designated HTML template, 'jokes.html' . Then will the setup and punchline values to the template
     # use the keys "setup" and "punchline" to access the value associated with that key -- this is the informat we want to display!
 
     # Ex: setup=a_joke["setup"]
-    return render_template('joke.html', setup= a_joke[...], punchline=a_joke[...])
+    return render_template('joke.html', setup= a_joke["setup"], punchline=a_joke["punchline"]
 
 
 @app.route("/many-jokes")
 def many_jokes():
     # intialize a variable ten_jokes and call the API function get_10_jokes() (TAKE NOTE OF THE TYPE IT RETURNS)
-    ten_jokes: list[dict[str, str]] = ...
+    ten_jokes: list[dict[str, str]] = get_10_jokes()
 
     # Now, we'll render the designated HTML template, 'many-jokes.html' . Then will the setup and punchline values to the template
     # This time, we'll pass in the whole list / ten_jokes variable to the template
 
     return render_template('many-jokes.html', jokes=ten_jokes)
-
-
-@app.route("/definition")
-def my_definitions():
-    # Right now, we are just returning the plain dictionary
-    # Look at the jokes example we did earlier if you want to experiment with reformatting the data!
-    # Read the Oxford Dictionary API documentation for more information on how to use the API!
-    a_definition: dict[str, str] = get_a_def()
-
-    # the dictionary returned from this API call is a lot more complicated than previous examples we've shown, notice the keys and indicing below
-    return render_template('definition.html', word=a_definition['id'], definition=a_definition['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-
 
 # API CALLS
 
@@ -58,11 +46,11 @@ def get_a_joke() -> dict[str, str]:
     # don't worry about the type, Python will take care of this
 
     # use requests.get() and pass in the API URL variable -- this will fetch a JSON
-    data = ...
+    data = resuests.get(jokes_api_url)
 
     # call .json() on the data variable - this will parse the JSON to a dict[str,str]
     # be careful of json structure -- sometimes it can be formatted within a list!
-    response: dict[str, str] = ...
+    response: dict[str, str] = data.json()
 
     return response
 
@@ -77,11 +65,11 @@ def get_10_jokes() -> list[dict[str, str]]:
     # don't worry about the type, Python will take care of this
 
     # use requests.get() and pass in the API URL variable -- this will fetch a JSON
-    data = ...
+    data = requests.get(jokes_api_url)
 
     # # call .json() on the data variable - this will parse the JSON
     # The response JSON is now a LIST of dictionaries! It's important to know the structure of response JSON!
-    response: list[dict[str, str]] = ...
+    response: list[dict[str, str]] = data.json()
 
     return response
 
@@ -99,7 +87,7 @@ def get_pictures() -> dict[str, str]:
     data = requests.get(url)
 
     # call .json() on data
-    response = ...
+    response = data.json()
 
     return response
 
@@ -114,7 +102,7 @@ def my_pictures():
     num = random.randint(1, 3)
 
     # Call get_pictures()
-    pictures: dict[str, str] = ...
+    pictures: dict[str, str] = get_pictures()
     # the dictionary returned from this API call is a lot more complicated than previous examples we've shown, notice the keys and indicing below
     return render_template('mars.html', source=pictures['photos'][num]['img_src'], date=pictures['photos'][num]['earth_date'])
 
